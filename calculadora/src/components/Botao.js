6a2983865faa24d9c638eae4d94ac80032ffc5d2
lealemao <leonjetix126@gmail.com>
2024-06-04 20:23:28 -0300

    Estilizações feitas no botao.js e no visor.js

--- calculadora/src/components/Botao.js
+++ calculadora/src/components/Botao.js
@@ -3,9 +3,15 @@ import { Text, TouchableHighlight, StyleSheet, Dimensions } from "react-native"
 const{width, height} = Dimensions.get("window")
 
 export default props => {
+
+    const estilosBotao = [estilos.botao]
+    if(props.dobro) estilosBotao.push(estilos.botaoDuplo)
+    if(props.triplo) estilosBotao.push(estilos.botaoTriplo)
+    if(props.corOp) estilosBotao.push(estilos.corOperacao)
+
     return(
         <TouchableHighlight>
-            <Text style={estilos.botao}>{props.label}</Text>
+            <Text style={estilosBotao}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
@@ -18,7 +24,18 @@ const estilos = StyleSheet.create({
         padding: 10,
         textAlign: 'center',
         borderWidth: 1,
-        borderColor: "#888",
+        borderColor: "#000",
         backgroundColor: "#f0f0f0"
+    },
+    botaoDuplo: {
+        width: width / 4 * 2
+    },
+    botaoTriplo: {
+        width: width / 4 * 3
+    },
+    corOperacao: {
+        backgroundColor: "#00a6ff",
+        borderColor: "#000",
+        color: "#fff"
     }
 })
